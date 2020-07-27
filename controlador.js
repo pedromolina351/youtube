656aa1e2320fb251eb7fe288e6ed3d4709fbfb2f
pedromolina351 <68834880+pedromolina351@users.noreply.github.com>
2020-07-26 21:27:32 -0600

    Add files via upload

---
+++ controlador.js
@@ -0,0 +1,21 @@
+var videos = [
+    {
+        urlImagen = 'img/1.jpg',
+        titulo = 'Cowboy Bebop OST mix',
+        canal = 'Terminal Passage',
+        visualizaciones = 3425680,
+        fecha = 'Hace 1 mes'
+
+    },
+    {
+        urlImagen = 'img/2.jpg',
+        titulo = 'El Ciudadano Kane de los videojuegos no existe | Post script',
+        canal = 'Dayo Script',
+        visualizaciones = 426259,
+        fecha = 'hace 2 semanas'
+    }
+];
+
+function generarMiniaturas(){
+    document.getElementById('videos').innerHTML =
+}
